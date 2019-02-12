16f1dcb6e50027804456feb5407889443e5b1abf
UrgenRommel <38016985+UrgenRommel@users.noreply.github.com>
2019-02-12 20:36:27 +0200

    Add files via upload

---
+++ 2 sem/lab 1/laba1.js
@@ -0,0 +1,16 @@
+var time =  234245645335;
+var ost  = time % 3600;
+ost
+console.log(ost);
+
+
+var stroka = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at";
+
+var l = [];
+for (i=0; i < stroka.length; i++){
+    if (stroka.charAt(i) == "a"){
+        l.push(i);
+    }
+}
+
+console.log (l);

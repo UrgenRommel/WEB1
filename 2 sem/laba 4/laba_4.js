6e32640a0b62393ce996856167df976097f4456c
UrgenRommel <38016985+UrgenRommel@users.noreply.github.com>
2019-03-19 11:34:32 +0200

    Add files via upload

---
+++ 2 sem/laba 4/laba_4.js
@@ -0,0 +1,8 @@
+document.getElementById("r4").addEventListener("click", clicked);
+function clicked() {
+  var in1 = document.getElementById("r1").value;
+  var in2 = document.getElementById("r2").value;
+  var in3 = document.getElementById("r3").value;
+
+  console.log(in1, in2, in3);
+}
